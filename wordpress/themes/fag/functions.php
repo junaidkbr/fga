79b5f55e9b10a2c6a558eee16e976bf68cbc131d
Junaid Ahmed <junaidkbr@gmail.com>
2022-09-21 13:40:31 +0500

    Adds static html

--- wordpress/themes/fag/functions.php
+++ wordpress/themes/fag/functions.php
@@ -1,1 +1,47 @@
 <?php
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ */
+function fag_theme_setup() {
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'script',
+			'style',
+			'navigation-widgets',
+		)
+	);
+}
+add_action( 'after_setup_theme', 'fag_theme_setup' );
+
+/**
+ * Enqueue Styles and Scripts.
+ */
+function fag_enqueue_assets() {
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	wp_enqueue_style( 'fag', get_stylesheet_directory_uri() . '/assets/css/styles.css', array(), $theme_version );
+}
+
+add_action( 'wp_enqueue_scripts', 'fag_enqueue_assets' );
