6658dc0710f4405e66b051e4170381b08ba066a0
Junaid Ahmed <junaidkbr@gmail.com>
2022-09-21 13:46:33 +0500

    Adds thank-you page template

---
+++ wordpress/themes/fga/page-templates/page-thank-you.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Thank You
+ */
+?>
+<?php get_header(); ?>
+
+  <div class="content-wrapper__column content-middle">
+    <section class="page-welcome-title thank-you">
+      <h1 class="page-welcome-title__heading-md">
+        <span class="text-red">Thank You!</span>
+        <br>
+        Your registration datais received.
+      </h1>
+
+      <hr class="help-box__seperator">
+
+      <p class="page-welcome-title__paragraph narrow text-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </section>
+  </div>
+
+<?php get_footer(); ?>
