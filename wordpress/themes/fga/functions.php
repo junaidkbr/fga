fd0dac6a5abc4d0ab552c36911cc8e4c30505c14
Junaid Ahmed <junaidkbr@gmail.com>
2022-09-24 04:10:59 +0500

    Removes gutenberg CSS

--- wordpress/themes/fga/functions.php
+++ wordpress/themes/fga/functions.php
@@ -58,6 +58,16 @@ function fga_enqueue_assets() {
 add_action( 'wp_enqueue_scripts', 'fga_enqueue_assets' );
 
 /**
+ * Removes Gutenberg Block Library CSS
+ */
+function fga_remove_wp_block_library_css(){
+  wp_dequeue_style( 'wp-block-library' );
+  wp_dequeue_style( 'wp-block-library-theme' );
+  wp_dequeue_style( 'wc-blocks-style' );
+ }
+add_action( 'wp_enqueue_scripts', 'fga_remove_wp_block_library_css', 100 );
+
+/**
  * Plugin dependency class
  */
 require get_template_directory() . '/inc/class-tgm-plugin-activation.php';
